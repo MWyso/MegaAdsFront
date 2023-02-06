cfec718e7cc9e843eb90b87da84082343223989f
MWyso <mateusz.wyso94@gmail.com>
2023-02-06 13:17:54 +0100

    Added and styled the map, install leaflet and configure OpenStreetMap

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet';
+import '../../utils/fix-map-icon'
+
+import 'leaflet/dist/leaflet.css';
 import './Map.css';
 
 export const Map = () => {
     return (
-        <div className="map"/>
-    );
-};
+        <MapContainer center={[52.2257495, 21.0071009]} zoom={13}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution="&copy; <a href='https://openstreetmap.org/copyright'>OpenStreetMap</a> & contributors"
+            />
+            <Marker position={[52.2257495, 21.0071009]}>
+                <Popup>
+                    <h2>Warszawa</h2>
+                    <p>Stolica Polski</p>
+                </Popup>
+            </Marker>
+            <Marker position={[50.26561, 18.9917058]}>
+                <Popup>
+                    <h2>IT.focus</h2>
+                    <p>Firma Jakuba MegaK</p>
+                </Popup>
+            </Marker>
+        </MapContainer>
+    )
+}
