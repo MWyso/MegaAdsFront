66ff2acfd03fea00c08d7f45f0fec44951b62a50
MWyso <mateusz.wyso94@gmail.com>
2023-02-14 08:49:31 +0100

    Added logo as a link to main page.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,8 @@
-import React, {FormEvent, SyntheticEvent, useContext, useState} from 'react';
+import React, {FormEvent, useContext, useState} from 'react';
 import { Btn } from '../common/Btn';
 import './Header.css';
 import {SearchContext} from "../../contexts/search.context";
+import { Logo } from '../Logo/Logo';
 
 export const Header = () => {
     const {search, setSearch} = useContext(SearchContext);
@@ -15,9 +16,7 @@ export const Header = () => {
 
     return (
         <header>
-            <h1>
-                <strong>Mega </strong> Ogłoszenia
-            </h1>
+            <Logo/>
             <Btn to="/add" text="Dodaj ogłoszenie"/>
             <form className="search" onSubmit={setSearchFromLocalState}>
                 <input type="text" value={inputVal} onChange={e => setInputVal(e.target.value)}/> <Btn text="Szukaj"/>
